ba0139c8b4e16ee33fc875717db41fa6fb8c63e7
Moroy Irina <irina.moroy@gmail.com>
2021-06-06 17:04:35 +0300

    New project traffic light

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict"
+let circle = document.getElementsByClassName("circle");
+console.log(circle);
+
+let activeLight = document.getElementById('colorRed').style.background = 'red';
+
+const redLight = document.getElementById('colorRed');
+const yellowLight = document.getElementById('colorYellow');
+const greenLight = document.getElementById('colorGreen');
+const butn = document.getElementById('btn');
+
+butn.addEventListener('click', function() {
+  
+})
